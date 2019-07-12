daf05ae7a1399288acfb24e72431c0a6f10a4586
RonnyRitscher <r.k.ritscher@web.de>
2019-07-12 15:07:05 +0200

    Initial commit

---
+++ app/src/main/java/de/ritscherappdeveloper/projekt_codesnippetcollectionapp/BorderWithLableActivity.java
@@ -0,0 +1,22 @@
+package de.ritscherappdeveloper.projekt_codesnippetcollectionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Border with Lable Demo
+ * 1. erstellen des "Shape" unter "drawable"
+ * 2. GUI - Relative-Layout inkl. EditText(Usereingabe) und TextView(LableText) in der .xml erstellen
+ * 3.
+ */
+
+public class BorderWithLableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_border_with_lable);
+
+
+    }
+}
---
+++ app/src/main/java/de/ritscherappdeveloper/projekt_codesnippetcollectionapp/MainActivity.java
@@ -0,0 +1,19 @@
+package de.ritscherappdeveloper.projekt_codesnippetcollectionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Inhaltsverzeichnis der CodeSnippets:
+ * #BorderWithLable
+ * #
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
